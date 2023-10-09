420e730901ae4a8d18b38a7283d577c18e942a6e
officemeets <146455791+officemeets@users.noreply.github.com>
2023-10-09 20:05:18 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -125,7 +125,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           centerTitle: true,
           leading: leading,
           elevation: 0.0,
-          title: Text("Hey There Vicky!"),
+          title: Text("Hey There Vicky! Whats up Bro"),
           backgroundColor: Colors.transparent,
           actions: actions,
         ),
